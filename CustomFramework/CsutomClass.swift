9a45b35510abd92ba05683539905d761707a0cd9
kevin <kevin@advmeds.com>
2020-07-07 11:57:26 +0800

    initial commit

---
+++ CustomFramework/CsutomClass.swift
@@ -0,0 +1,14 @@
+//
+//  CsutomClass.swift
+//  CustomFramework
+//
+//  Created by Administrator on 2020/7/7.
+//  Copyright © 2020 Kevin. All rights reserved.
+//
+
+import Foundation
+
+open class CsutomClass: NSObject {
+    
+    func textfunction() { NSLog("testLog") }
+}
---
+++ CustomFramework/CustomFramework.h
@@ -0,0 +1,19 @@
+//
+//  CustomFramework.h
+//  CustomFramework
+//
+//  Created by Administrator on 2020/7/7.
+//  Copyright © 2020 Kevin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomFramework.
+FOUNDATION_EXPORT double CustomFrameworkVersionNumber;
+
+//! Project version string for CustomFramework.
+FOUNDATION_EXPORT const unsigned char CustomFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomFramework/PublicHeader.h>
+
+
